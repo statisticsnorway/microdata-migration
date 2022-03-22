35594b5a43e3062eb683c4ea5213591883786bf9
Vassilios Kalantzakos <vassilios.kalantzakos@ssb.no>
2022-03-22 12:02:30 +0100

    Renames all metadata-all files.
    Example: from metadata_all__9.1.0.0.json to metadata_all__9_1_0.json

---
+++ migration_tools/migration/metadata/rename_metadata_all.py
@@ -0,0 +1,27 @@
+import os
+
+""" Renames all metadata-all files in given directory:
+    From metadata_all__14.0.0.0.json to metadata_all__14_0_0.json 
+"""
+
+def to_underscored_version(version: str) -> str:
+    if version.count('.') > 2:
+        version = '.'.join(version.split('.')[:-1])
+    version = version.replace('.', '_')
+    return version
+
+
+def main():
+
+    folder = "/Users/vak/temp/metadata-all-prod"
+    for count, filename in enumerate(os.listdir(folder)):
+        version = filename.removeprefix("metadata_all__").removesuffix(".json")
+        src =f"{folder}/{filename}"
+        dst =f"{folder}/{'metadata_all__'}{to_underscored_version(version)}.json"
+        print (src)
+        print (dst)
+        os.rename(src, dst)
+
+
+if __name__ == '__main__':
+    main()
