d5e9fbc114084bf276d77d8c0035c4d07ec1f329
Kalantzakos, Vassilios <Vassilios.Kalantzakos@ssb.no>
2022-06-10 16:25:42 +0200

    Script to extract unit types in csv format.

--- migration_tools/migration/metadata/create_unit_type_info.py
+++ migration_tools/migration/metadata/create_unit_type_info.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-"""Takes a file with all database table names and creates a json file with a unique list
+"""Takes a file with all database table names and creates a text file with a unique list
    of all variables and their unit types.
     
    Example database tables input file:
@@ -10,20 +10,11 @@ import json
      AFPO2011FDT_GRAD__3_0
      ....
      
-   Example of output file:  
-     [
-          {
-            "name": "AFPO1992FDT_MOTTAK",
-            "identifier": "PERSON",
-            "measure": null
-          },
-          {
-            "name": "ARBEIDSFORHOLD_PERSON",
-            "identifier": "JOBB",
-            "measure": "PERSON"
-          }
-     ] 
+   Example of output file:
    
+    AFPO1992FDT_MOTTAK__1_0, ident.UnitType: PERSON, measure.UnitType: None, measure.format: None
+    AFPO2011FDT_GRAD__1_0, ident.UnitType: PERSON, measure.UnitType: None, measure.format: None
+    AFPO2011FDT_GRAD__3_0, ident.UnitType: PERSON, measure.UnitType: None, measure.format: None
    
     Parameters
     ----------
@@ -41,14 +32,14 @@ import json
 
 # ------------- runtime parameters --------------
 database_tables = "/Users/vak/temp/pseudo-split/raird_all_tables-db-p1_2022-06-01.txt"
-get_from_prod = True
-output_file = "/Users/vak/temp/pseudo-split/raird_all_tables_unit_type_info.json"
+get_from_prod = False
+output_file = "/Users/vak/temp/pseudo-split/raird_all_tables_unit_type_info.txt"
 # ------------- runtime parameters --------------
 
 if get_from_prod:
-    fdbmetadata_url = "http://pl-raird-app-p2.ssb.no:8085/v.0.2/catalogs/RAIRD/dataSets/<dataset_name>"
+    fdbmetadata_url = "http://pl-raird-app-p2.ssb.no:8085/v.0.2/catalogs/RAIRD/dataSets/<dataset_name>?version=<dataset_version>"
 else:
-    fdbmetadata_url = "http://pl-raird-app-qa2.ssb.no:8085/v.0.2/catalogs/RAIRD/dataSets/<dataset_name>"
+    fdbmetadata_url = "http://pl-raird-app-qa2.ssb.no:8085/v.0.2/catalogs/RAIRD/dataSets/<dataset_name>?version=<dataset_version>"
 
 with open(database_tables) as f:
     table_list = f.read().splitlines()
@@ -57,22 +48,29 @@ metadata = []
 
 for dataset in table_list:
     dataset_name = dataset.split('__')[0]
-    dataset_metadata: dict = requests.get(fdbmetadata_url.replace("<dataset_name>", dataset_name)).json()
+    dataset_version = f'{dataset.split("__")[1].replace("_", ".")}.0'
+    url = fdbmetadata_url.replace("<dataset_name>", dataset_name).replace("<dataset_version>", dataset_version)
+
+    dataset_metadata: dict = requests.get(url).json()
 
     if len(dataset_metadata) == 0:
-        print(f'{dataset_name} : FDBMetadata returned empty json.')
+        print(f'{dataset} : FDBMetadata returned empty result.')
         continue
 
-    if not any(d['name'] == dataset_name for d in metadata):
-        identifier_unit_type = dataset_metadata.get('identifier')[0].get('unitType').get('name')
-        if dataset_metadata.get('measure').get('unitType'):
-            measure_unit_type = dataset_metadata.get('measure').get('unitType').get('name')
-        else:
-            measure_unit_type = None
+    identifier_unit_type = dataset_metadata.get('identifier')[0].get('unitType').get('name')
 
-        metadata.append(
-            {"name": dataset_name, "identifier": identifier_unit_type, "measure": measure_unit_type}
-        )
+    if dataset_metadata.get('measure').get('unitType'):
+        measure_unit_type = dataset_metadata.get('measure').get('unitType').get('name')
+    else:
+        measure_unit_type = None
 
-with open(output_file, "w") as f:
-    json.dump(metadata, f, indent=2)
+    if dataset_metadata.get('measure').get('format'):
+        measure_format = dataset_metadata.get('measure').get('format')
+    else:
+        measure_format = None
+
+    metadata.append(f'{dataset}, ident.UnitType: {identifier_unit_type}, '
+                    f'measure.UnitType: {measure_unit_type}, measure.format: {measure_format}')
+
+with open(output_file, 'w') as f:
+    f.write('\n'.join(metadata))
